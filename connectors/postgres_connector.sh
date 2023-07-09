7b4003a7f3bd62d4a47980cf1b73cc9f8dee6198
jacob <jyablonski9@gmail.com>
2023-07-09 09:56:12 -0500

    Reformat Repo (#78)

--- connectors/postgres_connector.sh
+++ connectors/postgres_connector.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# postgres - AVRO + schema registry gahbage for parquet sink basically
 curl -i -X PUT -H "Content-Type:application/json" \
   http://localhost:8083/connectors/source-debezium-postgres/config \
   -d '{
@@ -29,4 +30,36 @@ curl -i -X PUT -H "Content-Type:application/json" \
         "value.converter": "io.confluent.connect.avro.AvroConverter",
         "value.converter.enhanced.avro.schema.support": "true",
         "value.converter.schema.registry.url": "http://schema-registry:8081"
+    }'
+
+# postgres - JSON
+curl -i -X PUT -H "Content-Type:application/json" \
+  http://localhost:8083/connectors/source-debezium-postgres/config \
+  -d '{
+        "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+        "tasks.max": "1",
+        "database.hostname": "postgres",
+        "database.port": "5432",
+        "database.user": "postgres",
+        "database.password": "postgres",
+        "database.dbname" : "postgres",
+        "database.server.name": "asgard_postgres",
+        "schema.include.list": "dbz_schema",
+        "table.whitelist": "dbz_schema.second_movies",
+        "plugin.name": "pgoutput",
+        "include.schema.changes": "false",
+        "snapshot.mode": "never",
+        "transforms": "unwrap,dropTopicPrefix",
+        "transforms.unwrap.type": "io.debezium.transforms.ExtractNewRecordState",
+        "transforms.unwrap.drop.tombstones": "true",
+        "transforms.unwrap.delete.handling.mode":"rewrite",
+        "transforms.dropTopicPrefix.type":"org.apache.kafka.connect.transforms.RegexRouter",
+        "transforms.dropTopicPrefix.regex":"asgard_postgres.dbz_schema.(.*)",
+        "transforms.dropTopicPrefix.replacement":"postgres.$1",
+        "key.converter": "org.apache.kafka.connect.json.JsonConverter",
+        "key.converter.schemas.enable": "false",
+        "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+        "value.converter.schemas.enable": "false",
+        "log.retention.hours": "120",
+        "poll.interval.ms": "30000"
     }'
--- connectors/s3_sink.sh
+++ connectors/s3_sink.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# JSON
+# JSON with a couple of extra metadata columns attached
 curl -i -X PUT -H "Accept:application/json" \
     -H  "Content-Type:application/json" http://localhost:8083/connectors/jyablonski-kafka-s3-sink-mysql/config \
     -d '
@@ -26,6 +26,7 @@ curl -i -X PUT -H "Accept:application/json" \
 	}
 '
 
+# Parquet with a couple of extra metadata columns attached
 curl -i -X PUT -H "Accept:application/json" \
     -H  "Content-Type:application/json" http://localhost:8083/connectors/jyablonski-kafka-s3-sink-postgres/config \
     -d '
@@ -71,5 +72,39 @@ curl -i -X PUT -H "Accept:application/json" \
         "transforms": "InsertOffset",
         "transforms.InsertOffset.type": "org.apache.kafka.connect.transforms.InsertField$Value",
         "transforms.InsertOffset.offset.field": "source_offset"
+	}
+'
+
+# s3 sink with a `cdc_created_at`` transform to add the timestamp that debezium created the connectah
+curl -i -X PUT -H "Accept:application/json" \
+    -H  "Content-Type:application/json" http://localhost:8083/connectors/jyablonski-kafka-s3-sink-postgres/config \
+    -d '
+ {
+		"connector.class": "io.confluent.connect.s3.S3SinkConnector",
+		"key.converter":"org.apache.kafka.connect.json.JsonConverter",
+		"key.converter.schemas.enable": "false",
+		"value.converter.schemas.enable": "false",
+		"value.converter": "org.apache.kafka.connect.json.JsonConverter",
+		"tasks.max": "1",
+		"topics": "mysql.movies",
+		"s3.region": "us-east-1",
+		"s3.bucket.name": "jyablonski-kafka-s3-sink",
+        "rotate.schedule.interval.ms": "300000",
+        "timezone": "UTC",
+		"flush.size": "65536",
+		"storage.class": "io.confluent.connect.s3.storage.S3Storage",
+		"format.class": "io.confluent.connect.s3.format.json.JsonFormat",
+		"schema.generator.class": "io.confluent.connect.storage.hive.schema.DefaultSchemaGenerator",
+		"schema.compatibility": "NONE",
+        "partitioner.class": "io.confluent.connect.storage.partitioner.DefaultPartitioner",
+        "transforms": "AddMetadata,insertTimestamp,formatTimestamp",
+        "transforms.insertTimestamp.timestamp.field":"cdc_created_at", 
+        "transforms.insertTimestamp.type":"org.apache.kafka.connect.transforms.InsertField$Value",
+        "transforms.formatTimestamp.type": "org.apache.kafka.connect.transforms.TimestampConverter$Value",
+        "transforms.formatTimestamp.field": "cdc_created_at",
+        "transforms.formatTimestamp.format": "yyyy-MM-dd HH:mm:ssX",
+        "transforms.formatTimestamp.target.type": "string",
+        "transforms.AddMetadata.type": "org.apache.kafka.connect.transforms.InsertField$Value",
+        "transforms.AddMetadata.offset.field": "_offset"
 	}
 '
